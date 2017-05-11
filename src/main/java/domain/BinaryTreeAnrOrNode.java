7bf95029a0dae464816b188caa772ba521a1301c
lilelr <12211104lilele>
2017-05-11 20:36:28 +0800

    将and or 二叉树 叶子节点的表达式封装成TCItem

--- src/main/java/domain/BinaryTreeAnrOrNode.java
+++ src/main/java/domain/BinaryTreeAnrOrNode.java
@@ -1,19 +1,29 @@
 package domain;
 
 import net.sf.jsqlparser.expression.Expression;
+import operation.TCItem;
 
 /**
  * Created by yuxiao on 5/10/17.
  * 封装 选择操作元素的二叉树的节点，叶子节点为对应的选择表达式，非叶子节点为对应的and , or 操作
  */
 public class BinaryTreeAnrOrNode {
-    private Expression mExp;
+    private Expression mExp;// 记录叶子节点的表达式
+    private TCItem mcItem; // 将叶子节点的表达式封装成TCItem
     public BinaryTreeAnrOrNode left;
     public BinaryTreeAnrOrNode right;
 
-    public boolean isAndExp;
-    public boolean isOrExp;
-    public boolean isLeave;
+    public boolean isAndExp; //and
+    public boolean isOrExp;  // or
+    public boolean isLeave; // 是否是叶子节点
+
+    public TCItem getMcItem() {
+        return mcItem;
+    }
+
+    public void setMcItem(TCItem mcItem) {
+        this.mcItem = mcItem;
+    }
 
     public Expression getmExp() {
         return mExp;
