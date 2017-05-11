5d527ac8d0f208bd527cfe04160ea617a45e0d2d
lilelr <12211104lilele>
2017-05-11 16:33:39 +0800

    小修改

--- src/main/java/operation/Selection.java
+++ src/main/java/operation/Selection.java
@@ -2,6 +2,7 @@ package operation;
 
 import domain.BinaryTreeAndOr;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,7 +10,7 @@ import java.util.List;
  * 封装select tab1.a,tabl.b,tab2.A from tab1,tab2 where tab1.a>1000 and tabl.b<22语句中的选择操作
  */
 public class Selection {
-    public static List<String> fromTableList = null; // from 后面的表名list
+    public static List<String> fromTableList = new ArrayList<String>(); // from 后面的表名list
     public static BinaryTreeAndOr binaryTreeAndOr = null; // and 和 or 表达式组成的二叉树
 
 }
