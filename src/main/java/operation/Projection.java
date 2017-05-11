656f134b6f503bef79cc3e5bb99c440dc482f3b7
lilelr <12211104lilele>
2017-05-11 14:15:05 +0800

    获取selectItem操作语义

--- src/main/java/operation/Projection.java
+++ src/main/java/operation/Projection.java
@@ -1,12 +1,15 @@
 package operation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by yuxiao on 5/11/17.
- *  封装select tab1.a,tabl.b,tab2.A 语句中的映射
+ *  封装select tab1.a,tabl.b,tab2.A limit 1,20语句中的映射
  */
 public class Projection {
-    public static List<TCItem> proList=null; //所要映射元素的集合
+    public static List<TCItem> proList= new ArrayList<TCItem>(); //所要映射元素的集合
+    public static int limitStart=1; //SQL 语句中的limit 选择限制
+    public static int limitEnd =1000;
 
 }
