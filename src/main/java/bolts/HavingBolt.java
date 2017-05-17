a42e3262a89d3d1a467d5fffec27d990bad5bb22
lilelr <12211104lilele>
2017-05-17 17:25:34 +0800

    groupBy  和 having 代码修改

--- src/main/java/bolts/HavingBolt.java
+++ src/main/java/bolts/HavingBolt.java
@@ -1,5 +1,7 @@
 package bolts;
 
+import operation.AggregationStream;
+import operation.TCItem;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
@@ -26,8 +28,47 @@ public class HavingBolt extends BaseBasicBolt {
     @Override
     public void execute(Tuple input, BasicOutputCollector collector) {
 
-        System.out.println(input.size());
-        collector.emit(input.getValues());
+//        System.out.println(input.size());
+        // 获取这次tuple的表名
+        String curTableName = input.getStringByField("Table");
+        if (!AggregationStream.havingList.isEmpty()){
+            // SQL 有having 需求
+            boolean isPassing = false;
+            for (TCItem tcItem:AggregationStream.havingList){
+                if (curTableName.equals(tcItem.getTableName())){
+                    // 要比较属性的表名相同
+                    // 从当前tuple中获取表达式左边待比较的值
+                    Integer compLeftVal = Integer.valueOf((String) input.getValueByField(tcItem.getColName()));
+                    Integer compRightVal = tcItem.getComIntVal();
+                    // 获取比较符号 >,<,=,!=
+                    String compOperator = tcItem.getComparator();
+                    if (compOperator.equals(">")) {
+                        if (compLeftVal > compRightVal) {
+                            isPassing = true;
+                        }
+                    } else if (compOperator.equals("<")) {
+                        if (compLeftVal < compRightVal) {
+                            isPassing = true;
+                        }
+
+                    } else if (compOperator.equals("=")) {
+                        if (compLeftVal.equals(compRightVal) ) {
+                            isPassing = true;
+                        }
+                    } else if (compOperator.equals("!=")) {
+                        if (!compLeftVal.equals(compRightVal)) {
+                            isPassing = true;
+                        }
+                    }
+                }
+            }
+            if (isPassing){
+                collector.emit(input.getValues());
+            }
+        }else{
+            // SQL 没有having 需求，直接流过
+            collector.emit(input.getValues());
+        }
     }
 
     @Override
